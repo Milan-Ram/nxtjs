a4584030f25469bf119c9b5b2fcdb0bf8b17aa9b
Milan Ram <milanram0780@gmail.com>
2023-06-25 10:41:37 -0700

    cloufdlare deploy

--- nxtjob/app/Components/MainLayoutComps/JobSidebar.js
+++ nxtjob/app/Components/MainLayoutComps/JobSidebar.js
@@ -2,13 +2,22 @@
 import React from 'react'
 import Image from 'next/image'
 
-
 const JobSidebar = () => {
-
+  
   const [ active , setActive] =React.useState('boards')
 
   return (
-    <div className=' min-w-[213px] w-full h-fit  text-black text-[14px] py-[20px] px-[10px] bg-white rounded-[12px] flex flex-col gap-[10px]'>
+    <div
+  animate={{
+    x: 0,
+    backgroundColor: "#000",
+    boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
+    position: "fixed",
+    transitionEnd: {
+      display: "none",
+    },
+  }}
+ className='item min-w-[213px] w-full h-fit  text-black text-[14px] py-[20px] px-[10px] bg-white rounded-[12px] flex flex-col gap-[10px]'>
     <div  className={`flex items-center gap-2 cursor-pointer  ${active === "boards" ? "bg-[#8246FD]" : "bg-[#fff"}  ${active === "boards" ? "text-[#fff]" : "text-[#000"} font-[600] text-[12px] p-[10px] rounded-[8px]`}     onClick={()=>{setActive('boards')}} ><Image src="/sideBarIcons/squares.png" width={32} height={32} alt="icon" />Boards</div>
     <div  className={`flex items-center gap-2 cursor-pointer  ${active === "networking" ? "bg-[#8246FD]" : "bg-[#fff"}  ${active === "networking" ? "text-[#fff]" : "text-[#000"} font-[600] p-[10px] text-[12px] text-[#212121] rounded-[8px]`} onClick={()=>{setActive('networking')}}><Image src="/sideBarIcons/team.png" width={32} height={32} alt="icon" />  Networking</div>
     <div  className={`flex items-center gap-2 cursor-pointer ${active === "archive" ? "bg-[#8246FD]" : "bg-[#fff"}  ${active === "archive" ? "text-[#fff]" : "text-[#000"} font-[600] p-[10px] text-[12px] text-[#212121] rounded-[8px]`} onClick={()=>{setActive('archive')}}><Image src="/sideBarIcons/archive.png" width={32} height={32} alt="icon" />   Archive Jobs</div>
