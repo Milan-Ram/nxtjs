e5883f3dc18a1aea69c1632d68d0a38805096969
Milan Ram <milanram0780@gmail.com>
2023-06-25 11:42:49 -0700

    last2

--- nxtjob/app/Components/MainLayoutComps/NavigationMenu.js
+++ nxtjob/app/Components/MainLayoutComps/NavigationMenu.js
@@ -133,6 +133,22 @@ const NavigationMenuComponent = () => {
   );
 };
 
-
+const ListItem = React.forwardRef(({ className, children, title, ...props }, forwardedRef) => (
+  <li>
+    <NavigationMenu.Link asChild>
+      <a
+        className={classNames(
+          ' focus:shadow-violet7 hover:bg-mauve3 block select-none rounded-[6px] p-3  text-[14px] leading-none no-underline outline-none transition-colors',
+          className
+        )}
+        {...props}
+        ref={forwardedRef}
+      >
+        <div className="text-violet12 mb-[5px] font-medium leading-[1.2]">{title}</div>
+        <p className="text-mauve11 leading-[1.4]">{children}</p>
+      </a>
+    </NavigationMenu.Link>
+  </li>
+));
 
 export default NavigationMenuComponent;
--- nxtjob/app/Components/selectComp.js
+++ nxtjob/app/Components/selectComp.js
@@ -42,6 +42,22 @@ const SelectComp = () => {
   </Select.Root>
 );
   }
-
+const SelectItem = React.forwardRef(({ children, className, ...props }, forwardedRef) => {
+  return (
+    <Select.Item
+      className={classnames(
+        'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
+        className
+      )}
+      {...props}
+      ref={forwardedRef}
+    >
+      <Select.ItemText>{children}</Select.ItemText>
+      <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+        <CheckIcon />
+      </Select.ItemIndicator>
+    </Select.Item>
+  );
+});
 
 export default SelectComp;
